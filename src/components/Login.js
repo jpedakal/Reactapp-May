872f04fd7ca843d208c29f98b6c5b74034a908b5
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-05-01 16:22:46 +0530

    added login form

---
+++ src/components/Login.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div className="container">
+            <h3>Please Login</h3>
+            <div className="form-group">
+                <label>Email:</label>
+                <input type="email" className="form-control"></input>
+            </div>
+            <div className="form-group">
+                <label>Password:</label>
+                <input type="password" className="form-control"></input>
+            </div>
+            <button type="button" className="btn btn-success">Login</button>
+        </div>
+    )
+}
+
+export default Login;
