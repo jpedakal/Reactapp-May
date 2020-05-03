8568b6e95e0ec850f75570e74dc6fd67f5d56228
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-05-03 10:38:17 +0530

    update

--- src/components/Routing.js
+++ src/components/Routing.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Route, BrowserRouter, Link } from 'react-router-dom';
-import Home from './Home';
 import dashboard from './Dashboard';
 import register from './Register';
 import login from './Login';
@@ -14,14 +13,28 @@ import productData from './productData';
 const Routing = () => {
     return (
         <BrowserRouter>
-            <Route to="/" component={Home}></Route>
-            <Route to="/register" component={register}></Route>
-            <Route to="/login" component={login}></Route>
-            <Route to="/cart" component={cart}></Route>
-            <Route to="/myOrders" component={myOrders}></Route>
-            <Route to="/payment" component={payment}></Route>
-            <Route to="/paymentProcess" component={paymentProcess}></Route>
-            <Route to="/productData" component={productData}></Route>
+            <div>
+                <nav className="navbar navbar-custom">
+                    <div className="container-fluid">
+                        <div className="navbar-header">
+                            <a className="navbar-brand" href="/brand">Welcome To Carrefour</a>
+                        </div>
+                        <ul className="nav navbar-nav navbar-right">
+                            <li><span className="glyphicon glyphicon-shopping-cart"><Link to="/cart">Cart</Link></span></li>
+                            <li><span className="glyphicon glyphicon-log-in"><Link to="/login">Login</Link></span></li>
+                            <li><span className="glyphicon glyphicon-user"><Link to="/register">Register</Link></span></li>
+                        </ul>
+                    </div>
+                </nav>
+                <Route path="/register" component={register}></Route>
+                <Route path="/login" component={login}></Route>
+                <Route path="dashboard" component={dashboard}></Route>
+                <Route path="/cart" component={cart}></Route>
+                <Route path="/myOrders" component={myOrders}></Route>
+                <Route path="/payment" component={payment}></Route>
+                <Route path="/paymentProcess" component={paymentProcess}></Route>
+                <Route path="/productData" component={productData}></Route>
+            </div>
         </BrowserRouter>
     )
 }
