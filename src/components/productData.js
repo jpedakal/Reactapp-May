d8cff662494d8770dfbe5cfa625acca1b66ab1b3
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-05-03 07:04:27 +0530

    update

--- src/components/productData.js
+++ src/components/productData.js
@@ -13,7 +13,7 @@ const productData = () => {
         </div>
         <div className="row">
           <div className="col-md-2">Price</div>
-          <div id="pro-val"className="col-md-4">89999</div>
+          <div id="pro-val"className="col-md-4">Rs 89999</div>
         </div>
         <div className="row">
           <div className="col-md-2">Seller</div>
@@ -22,7 +22,11 @@ const productData = () => {
         <div className="row">
           <div className="col-md-2">Description</div>
           <div id="pro-val"className="col-md-9">If you are a travel blogger, gamer, entertainment seeker, or a person who loves a high-end personal device, then the Redmi 8 has been created to meet your needs. This smartphone features a 15.8-cm (6.22) Dot Notch Display, a 12 MP + 2 MP AI Dual Camera, and a 5000 mAh High-capacity Battery to offer detailed views of the stunning photos that you can click all day long without running out of battery life.</div>
-        </div>
+        </div><br /><br />
+        <div className="row">
+          <button type="button" id="pro-btn" className="btn btn-warning">ADD To CART</button>
+          <button type="button" id="pro-btn" className="btn btn-danger">BUY NOW</button>
+         </div>
       </div>
     </div>
   )
