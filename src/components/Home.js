d1a0ad2b5c5dd01ce03ca2da7ef07883e5750cf1
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-05-03 10:52:23 +0530

    update

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import { Route, BrowserRouter, Link } from 'react-router-dom';
 
 const Home = () => {
     return (
         <nav className="navbar navbar-custom">
             <div className="container-fluid">
                 <div className="navbar-header">
-                    <a className="navbar-brand" href="/brand">Welcome To Carrefour</a>
+                    <a className="navbar-brand" href="/">Welcome To Carrefour</a>
                 </div>
                 <ul className="nav navbar-nav navbar-right">
-                    <li><a href="/cart"><span className="glyphicon glyphicon-shopping-cart"></span> Cart</a></li>
-                    <li><a href="/register"><span className="glyphicon glyphicon-user"></span> Register</a></li>
-                    <li><a href="/login"><span className="glyphicon glyphicon-log-in"></span> Login</a></li>
+                    <li><Link to="/cart"><span className="glyphicon glyphicon-shopping-cart">Cart</span></Link></li>
+                    <li><Link to="/login"><span className="glyphicon glyphicon-log-in">Login</span></Link></li>
+                    <li><Link to="/register"><span className="glyphicon glyphicon-user">Register</span></Link></li>
                 </ul>
             </div>
         </nav>
--- src/components/Routing.js
+++ src/components/Routing.js
@@ -17,12 +17,12 @@ const Routing = () => {
                 <nav className="navbar navbar-custom">
                     <div className="container-fluid">
                         <div className="navbar-header">
-                            <a className="navbar-brand" href="/brand">Welcome To Carrefour</a>
+                            <div className="title">Welcome To Carrefour</div>
                         </div>
                         <ul className="nav navbar-nav navbar-right">
-                            <li><span className="glyphicon glyphicon-shopping-cart"><Link to="/cart">Cart</Link></span></li>
-                            <li><span className="glyphicon glyphicon-log-in"><Link to="/login">Login</Link></span></li>
-                            <li><span className="glyphicon glyphicon-user"><Link to="/register">Register</Link></span></li>
+                            <li><Link to="/cart"><span className="glyphicon glyphicon-shopping-cart">Cart</span></Link></li>
+                            <li><Link to="/login"><span className="glyphicon glyphicon-log-in">Login</span></Link></li>
+                            <li><Link to="/register"><span className="glyphicon glyphicon-user">Register</span></Link></li>
                         </ul>
                     </div>
                 </nav>
