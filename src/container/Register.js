7412e85728d5faeff3e05c9be9a55fa13bfea456
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-05-04 21:16:51 +0530

    Update Register.js

--- src/container/Register.js
+++ src/container/Register.js
@@ -2,45 +2,47 @@ import React, { Component } from 'react';
 
 class Register extends Component {
 
-  handleSubmit=(e)=>{
-      e.preventDefault();
-      const name= this.getName.value;
-      const email= this.getEmail.value;
-      const mobile= this.getMobile.value;
-      const password= this.getPwd.value;
-      const confirmPassword= this.getCnfpwd.value;
+    handleSubmit = (e) => {
+        e.preventDefault();
+        const name = this.getName.value;
+        const email = this.getEmail.value;
+        const mobile = this.getMobile.value;
+        const password = this.getPwd.value;
+        const confirmPassword = this.getCnfpwd.value;
 
-      const data= {
-          name, email, mobile, password, confirmPassword
-      }
+        const data = {
+            name, email, mobile, password, confirmPassword
+        }
 
-      console.log('data'+data)
-  }
+        console.log('data' + data)
+    }
     render() {
         return (
             <div className="container">
                 <h3>Please Register your Details</h3>
-                <div className="form-group">
-                    <label>Name:</label>
-                    <input type="text" className="form-control" ref={(input)=>this.getName=input}></input>
-                </div>
-                <div className="form-group">
-                    <label>Email:</label>
-                    <input type="email" className="form-control" ref={(input)=>this.getEmail=input}></input>
-                </div>
-                <div className="form-group">
-                    <label>Mobile:</label>
-                    <input type="number" className="form-control" ref={(input)=>this.getMobile=input}></input>
-                </div>
-                <div className="form-group">
-                    <label>Password:</label>
-                    <input type="password" className="form-control" ref={(input)=>this.getPwd=input}></input>
-                </div>
-                <div className="form-group">
-                    <label>Confirm Password:</label>
-                    <input type="password" className="form-control" ref={(input)=>this.getCnfpwd=input}></input>
-                </div>
-                <button type="submit" className="btn btn-success" onSubmit={this.handleSubmit}>Register</button>
+                <form onSubmit={this.handleSubmit} >
+                    <div className="form-group">
+                        <label>Name:</label>
+                        <input type="text" className="form-control" ref={(input) => this.getName = input}></input>
+                    </div>
+                    <div className="form-group">
+                        <label>Email:</label>
+                        <input type="email" className="form-control" ref={(input) => this.getEmail = input}></input>
+                    </div>
+                    <div className="form-group">
+                        <label>Mobile:</label>
+                        <input type="number" className="form-control" ref={(input) => this.getMobile = input}></input>
+                    </div>
+                    <div className="form-group">
+                        <label>Password:</label>
+                        <input type="password" className="form-control" ref={(input) => this.getPwd = input}></input>
+                    </div>
+                    <div className="form-group">
+                        <label>Confirm Password:</label>
+                        <input type="password" className="form-control" ref={(input) => this.getCnfpwd = input}></input>
+                    </div>
+                    <button type="submit" className="btn btn-success" >Register</button>
+                </form>
             </div>
         )
 
