d4df03be136ad33d8adb6e5d158b8cee351aa41d
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-05-05 15:04:53 +0530

    update

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Routing from './components/Routing';
+import store from './store/index';
+import { Provider } from 'react-redux';
 
 ReactDOM.render(
- <Routing/>,
+  <Provider store={store}>
+    <Routing />
+  </Provider>,
   document.getElementById('root')
 );
 
--- src/store/index.js
+++ src/store/index.js
@@ -2,3 +2,5 @@ import {createStore} from 'redux';
 import postReducer from '../reducer/index'
 
 const  store = createStore(postReducer);
+
+export default store;
