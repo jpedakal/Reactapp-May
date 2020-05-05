ffc3704beb067f2d1ca2e040bfd867b3cba37a4c
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-05-05 14:56:50 +0530

    Update index.js

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,5 +1,11 @@
 const postReducer = (state = [], action) => {
 
+    switch (action.type) {
+        case 'POST_REGISTER':
+            return state.concat([action.data]);
+        default:
+            return state;
+    }
 }
 
 export default postReducer;
