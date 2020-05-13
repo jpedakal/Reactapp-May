04580d1de741bf93881f009555b2a207df568bb0
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-05-13 11:14:48 +0530

    Update Login.js

--- src/container/Login.js
+++ src/container/Login.js
@@ -1,17 +1,28 @@
 import React from 'react';
 
 const Login = () => {
+
+    handleSubmit=(e)=>{
+        e.preventDefault();
+
+        const email= this.getEmail.value;
+        const password= this.getPassword.value;
+
+        const data= {email, password};
+        
+    }
+
     return (
         <div className="container">
             <h3>Please Login</h3>
             <form onSubmit={this.handleSubmit}>
                 <div className="form-group">
                     <label>Email:</label>
-                    <input type="email" className="form-control" required></input>
+                    <input type="email" className="form-control" ref={(input)=>this.getEmail=input} required></input>
                 </div>
                 <div className="form-group">
                     <label>Password:</label>
-                    <input type="password" className="form-control" required></input>
+                    <input type="password" className="form-control" ref={(input)=> this.getPassword=input} required></input>
                 </div>
                 <button type="button" className="btn btn-success">Login</button>
             </form>
