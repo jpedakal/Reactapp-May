867da3f0cfa80eaa4a90348cc3f2542be138ea8b
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-05-13 11:08:50 +0530

    Update Login.js

--- src/container/Login.js
+++ src/container/Login.js
@@ -4,15 +4,17 @@ const Login = () => {
     return (
         <div className="container">
             <h3>Please Login</h3>
-            <div className="form-group">
-                <label>Email:</label>
-                <input type="email" className="form-control" required></input>
-            </div>
-            <div className="form-group">
-                <label>Password:</label>
-                <input type="password" className="form-control" required></input>
-            </div>
-            <button type="button" className="btn btn-success">Login</button>
+            <form onSubmit={this.handleSubmit}>
+                <div className="form-group">
+                    <label>Email:</label>
+                    <input type="email" className="form-control" required></input>
+                </div>
+                <div className="form-group">
+                    <label>Password:</label>
+                    <input type="password" className="form-control" required></input>
+                </div>
+                <button type="button" className="btn btn-success">Login</button>
+            </form>
         </div>
     )
 }
