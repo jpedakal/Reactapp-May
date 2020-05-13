3cf6668513a4ddd6cd2a06f8deac7ea7ebefac84
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-05-13 20:52:07 +0530

    Update Routing.js

--- src/components/Routing.js
+++ src/components/Routing.js
@@ -21,9 +21,9 @@ const Routing = () => {
                             <Link to="/"><h3>Welcome To Carrefour</h3></Link>
                         </div>
                         <ul className="nav navbar-nav navbar-right">
-                            <li><Link to="/cart"><span className="glyphicon glyphicon-shopping-cart">Cart</span></Link></li>
-                            <li><Link to="/login"><span className="glyphicon glyphicon-log-in">Login</span></Link></li>
-                            <li><Link to="/register"><span className="glyphicon glyphicon-user">Register</span></Link></li>
+                            <li><Link to="/cart"><span className="glyphicon glyphicon-shopping-cart"> Cart</span></Link></li>
+                            <li><Link to="/login"><span className="glyphicon glyphicon-log-in"> Login</span></Link></li>
+                            <li><Link to="/register"><span className="glyphicon glyphicon-user"> Register</span></Link></li>
                         </ul>
                     </div>
                 </nav>
