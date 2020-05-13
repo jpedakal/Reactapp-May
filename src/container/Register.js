60a32205be66266d6dfb2500ad4894e69df31675
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-05-13 08:43:41 +0530

    Update Register.js

--- src/container/Register.js
+++ src/container/Register.js
@@ -5,7 +5,7 @@ import registerUser from '../action/index';
 class Register extends Component {
 
     handleSubmit = (e) => {
-        e.preventDefault();
+        
 
         let data;
         const name = this.getName.value;
@@ -28,23 +28,23 @@ class Register extends Component {
                 <form onSubmit={this.handleSubmit} >
                     <div className="form-group">
                         <label>Name:</label>
-                        <input type="text" className="form-control" ref={(input) => this.getName = input}></input>
+                        <input type="text"  className="form-control" ref={(input) => this.getName = input} required></input>
                     </div>
                     <div className="form-group">
                         <label>Email:</label>
-                        <input type="email" className="form-control" ref={(input) => this.getEmail = input}></input>
+                        <input type="email" className="form-control" ref={(input) => this.getEmail = input} required></input>
                     </div>
                     <div className="form-group">
                         <label>Mobile:</label>
-                        <input type="number" className="form-control" ref={(input) => this.getMobile = input}></input>
+                        <input type="number" className="form-control" ref={(input) => this.getMobile = input} required></input>
                     </div>
                     <div className="form-group">
                         <label>Password:</label>
-                        <input type="password" className="form-control" ref={(input) => this.getPwd = input}></input>
+                        <input type="password" className="form-control" ref={(input) => this.getPwd = input} required></input>
                     </div>
                     <div className="form-group">
                         <label>Confirm Password:</label>
-                        <input type="password" className="form-control" ref={(input) => this.getCnfpwd = input}></input>
+                        <input type="password" className="form-control" ref={(input) => this.getCnfpwd = input} required></input>
                     </div>
                     <button type="submit" className="btn btn-success" >Register</button>
                 </form>
